dbce39033b95204d8c3e0a56c0d6267223ac8113
swapnilukey1995 <83223911+swapnilukey1995@users.noreply.github.com>
2022-02-08 13:13:05 +0530

    Add files via upload
    useState, useReducer, useEffect, useRef are used in this app

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Setup from './components/Swapnil/Custom_Form/'
+function App() {
+    return (
+        <div className='container'>
+            <Setup />
+        </div>
+    )
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,107 @@
+import React, { useState, useReducer, useEffect, useRef } from 'react';
+import Modal from './model';
+import { reducer } from './reducer';
+
+const defaultState = {
+    people: [],
+    isModelOpen: false,
+    modalContent: ''
+}
+
+const Index = () => {
+
+    const refContainer = useRef(null)
+    const [person, setPerson] = useState({ firstName: '', email: '', age: '' })
+    const [state, dispatch] = useReducer(reducer, defaultState)
+
+    const handleChange = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        // console.log(name, value);
+        setPerson({ ...person, [name]: value })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (person.firstName && person.email && person.age) {
+            const newPerson = {
+                id: new Date().getTime().toString(),
+                ...person
+            }
+            dispatch({ type: 'ADD_ITEM', payload: newPerson });
+            setPerson({ firstName: '', email: '', age: '' })
+        }
+        else {
+            dispatch({ type: 'NO_VALUE' });
+        }
+    }
+    const closeModal = () => {
+        dispatch({ type: 'CLOSE_MODAL' });
+    };
+
+    useEffect(() => {
+        refContainer.current.focus()
+    }, [])
+
+    return (
+        <>
+            {/*  state.isModalOpen  is false  */}
+            {state.isModalOpen && (
+                <Modal closeModal={closeModal}
+                    modalContent={state.modalContent} />
+            )}
+            <form onSubmit={handleSubmit} className='form'>
+                <div className="form-control">
+                    <label htmlFor="firstName">Name:</label>
+                    <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        ref={refContainer}
+                        value={person.firstName}
+                        onChange={handleChange} />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        value={person.email}
+                        onChange={handleChange} />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="age">Age:</label>
+                    <input
+                        type="text"
+                        id="age"
+                        name="age"
+                        value={person.age}
+                        onChange={handleChange} />
+                </div>
+                <button type='submit'>Add </button>
+            </form>
+            {state.people.map((person) => {
+                return (
+                    <div key={person.id} className='item'>
+                        <h4>{person.firstName}</h4>
+                        <h4>{person.age}</h4>
+                        <h4>{person.email}</h4>
+                        <button
+                            onClick={() =>
+                                dispatch({
+                                    type: 'REMOVE_ITEM',
+                                    payload: person.id
+                                })
+                            }
+                        >
+                            remove
+                        </button>
+                    </div>
+                );
+            })}
+
+        </>
+    );
+};
+
+export default Index;
